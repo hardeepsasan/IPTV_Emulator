8259adf12aae71cc269ae86f904de50aa2dc1302
hardeepsasan <hardeepsasan@gmail.com>
2026-01-10 23:09:00 -0500

    Initial Commit

---
+++ IPTV_Emulator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IPTV_Emulator
+//
+//  Created by hardeepsasan on 1/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IPTV_Emulator/IPTV_EmulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPTV_EmulatorApp.swift
+//  IPTV_Emulator
+//
+//  Created by hardeepsasan on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct IPTV_EmulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
